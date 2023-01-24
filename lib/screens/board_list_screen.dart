d77e2e5fcf8cc50826241140729373ad26233291
radeonovich <radeonovich1@yandex.ru>
2023-01-24 19:38:34 +0300

    fix gray screen in release mode

--- lib/screens/board_list_screen.dart
+++ lib/screens/board_list_screen.dart
@@ -2,6 +2,25 @@ import 'package:flutter/material.dart';
 import 'board_screen.dart';
 
 void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    bool inDebug = false;
+    assert(() {
+      inDebug = true;
+      return true;
+    }());
+    // In debug mode, use the normal error widget which shows
+    // the error message:
+    if (inDebug) return ErrorWidget(details.exception);
+    // In release builds, show a yellow-on-blue message instead:
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Error! ${details.exception}',
+        style: TextStyle(color: Colors.yellow),
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  };
   runApp(const MyApp());
 }
 
