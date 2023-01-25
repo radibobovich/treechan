cc5976b7f444008ad0bb11ee0e62009a1bca96c6
radeonovich <radeonovich1@yandex.ru>
2023-01-25 21:00:38 +0300

    replace length-fixed image list to a growable list

--- lib/widgets/image_preview_widget.dart
+++ lib/widgets/image_preview_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:treechan/board_json.dart';
 
-class ImagesGroup extends StatelessWidget {
-  const ImagesGroup({Key? key, required this.files}) : super(key: key);
+/// Scrollable horizontal row with image previews.
+class ImagesPreview extends StatelessWidget {
+  const ImagesPreview({Key? key, required this.files}) : super(key: key);
 
   final List<File>? files;
   @override
@@ -24,12 +25,19 @@ List<Widget> _getImages(List<File>? files) {
     // return empty image list
     return List<Widget>.filled(1, const SizedBox.shrink());
   }
-  List<Widget> images = List<Widget>.filled(8, const SizedBox.shrink());
-  for (int i = 0; i < files.length; i++) {
-    if (files[i].thumbnail != null) {
-      images[i] =
-          Image.network(files[i].thumbnail!, height: 140, fit: BoxFit.contain);
+  //List<Widget> images = List<Widget>.filled(8, const SizedBox.shrink());
+  List<Widget> images = List<Widget>.empty(growable: true);
+  for (var file in files) {
+    if (file.thumbnail != null) {
+      images.add(
+          Image.network(file.thumbnail!, height: 140, fit: BoxFit.contain));
     }
   }
+  // for (int i = 0; i < files.length; i++) {
+  //   if (files[i].thumbnail != null) {
+  //     images[i] =
+  //         Image.network(files[i].thumbnail!, height: 140, fit: BoxFit.contain);
+  //   }
+  // }
   return images;
 }
