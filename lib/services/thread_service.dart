d9e888489839701247343913aa7429f54e3ffa42
radeonovich <radeonovich1@yandex.ru>
2023-01-25 22:45:01 +0300

    refactor

--- lib/services/thread_service.dart
+++ lib/services/thread_service.dart
@@ -3,10 +3,9 @@ import 'package:treechan/board_json.dart';
 import 'dart:convert';
 
 Future<Thread?> getThread(String tag, int threadId) async {
-  var url = "https://2ch.hk/" + tag + "/res/" + threadId.toString() + ".json";
+  var url = "https://2ch.hk/$tag/res/${threadId.toString()}.json";
   final response = await http.get(Uri.parse(url));
   if (response.statusCode == 200) {
-    var test = Root.fromJson(jsonDecode(response.body));
     var thread = Root.fromJson(jsonDecode(response.body)).threads?.first;
     thread?.posts?.forEach((post) {
       if (post.files != null) {
