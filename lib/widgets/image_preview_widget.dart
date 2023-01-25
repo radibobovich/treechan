eb62f49d4e69a3c63d74ea347dfe04a2d3045a12
radeonovich <radeonovich1@yandex.ru>
2023-01-25 21:43:20 +0300

    add image gallery

--- lib/widgets/image_preview_widget.dart
+++ lib/widgets/image_preview_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:treechan/board_json.dart';
+import 'package:swipe_image_gallery/swipe_image_gallery.dart';
 
 /// Scrollable horizontal row with image previews.
 class ImagesPreview extends StatelessWidget {
   const ImagesPreview({Key? key, required this.files}) : super(key: key);
-
   final List<File>? files;
   @override
   Widget build(BuildContext context) {
@@ -13,24 +13,30 @@ class ImagesPreview extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: _getImages(files),
+          children: _getImages(files, context),
         ),
       ),
     );
   }
 }
 
-List<Widget> _getImages(List<File>? files) {
+List<Widget> _getImages(List<File>? files, BuildContext context) {
   if (files == null) {
     // return empty image list
     return List<Widget>.filled(1, const SizedBox.shrink());
   }
   //List<Widget> images = List<Widget>.filled(8, const SizedBox.shrink());
   List<Widget> images = List<Widget>.empty(growable: true);
+  List<String> imageLinks = List<String>.empty(growable: true);
   for (var file in files) {
-    if (file.thumbnail != null) {
+    if (file.type == 1 || file.type == 2) {
+      imageLinks.add("https://2ch.hk${file.path ?? ""}");
+    }
+  }
+  for (var file in files) {
+    if (file.type == 1 || file.type == 2) {
       images.add(
-          Image.network(file.thumbnail!, height: 140, fit: BoxFit.contain));
+          ImagePreview(imageLinks: imageLinks, file: file, context: context));
     }
   }
   // for (int i = 0; i < files.length; i++) {
@@ -40,4 +46,30 @@ List<Widget> _getImages(List<File>? files) {
   //   }
   // }
   return images;
+}
+
+class ImagePreview extends StatelessWidget {
+  const ImagePreview(
+      {Key? key,
+      required this.imageLinks,
+      required this.file,
+      required this.context})
+      : super(key: key);
+
+  final List<String> imageLinks;
+  final File file;
+  final BuildContext context;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => SwipeImageGallery(
+                context: context,
+                itemBuilder: (context, index) {
+                  return Image.network(imageLinks[index]);
+                },
+                itemCount: imageLinks.length)
+            .show(),
+        child:
+            Image.network(file.thumbnail!, height: 140, fit: BoxFit.contain));
+  }
 }
