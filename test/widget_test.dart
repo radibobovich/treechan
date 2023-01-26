7093392a0ed67e73ff61409071bf3d6c17fe50e4
radeonovich <radeonovich1@yandex.ru>
2023-01-26 16:17:48 +0300

    add api.yml

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:treechan/main.dart';
 
 import 'package:treechan/screens/board_list_screen.dart';
 
