68778ce01f89479e04b5b9d0d930c608796fbe54
radeonovich <radeonovich1@yandex.ru>
2023-01-28 22:31:35 +0300

    refactor

--- lib/screens/thread_screen.dart
+++ lib/screens/thread_screen.dart
@@ -6,6 +6,7 @@ import '/services/thread_service.dart';
 //import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
 import 'package:flexible_tree_view/flexible_tree_view.dart';
 import '../widgets/image_preview_widget.dart';
+import '../services/tree_service.dart';
 
 class ThreadScreen2 extends StatefulWidget {
   const ThreadScreen2({super.key, required this.threadId, required this.tag});
@@ -33,35 +34,15 @@ class _ThreadScreen2State extends State<ThreadScreen2> {
               future: roots,
               builder: ((context, snapshot) {
                 if (snapshot.hasData) {
-                  return Expanded(
+                  return Flexible(
                     child: FlexibleTreeView<FormattedPost>(
+                      indent: 8,
+                      showLines: true,
                       nodes: snapshot.data!,
                       nodeItemBuilder: (context, node) {
-                        return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                            //child: PostWidget(post: node.data),
-                            child: Row(
-                              children: [
-                                node.hasNodes
-                                    ? IconButton(
-                                        iconSize: 12,
-                                        splashRadius: 16,
-                                        padding: EdgeInsets.zero,
-                                        constraints: BoxConstraints.tight(
-                                            const Size(30, 30)),
-                                        icon: Icon(node.expanded
-                                            ? Icons.remove
-                                            : Icons.add),
-                                        onPressed: () {
-                                          node.expanded = !node.expanded;
-                                        },
-                                      )
-                                    : const SizedBox(
-                                        width: 12,
-                                      ),
-                                PostWidget(post: node.data),
-                              ],
-                            ));
+                        return PostNode(
+                          node: node,
+                        );
                       },
                     ),
                   );
@@ -74,105 +55,47 @@ class _ThreadScreen2State extends State<ThreadScreen2> {
   }
 }
 
-class FormattedPost {
-  Post? postInfo;
-  List<int>? parents = List.empty(growable: true);
-  FormattedPost({this.postInfo, this.parents});
-}
+/// Represents post with expand/minimize button.
+class PostNode extends StatelessWidget {
+  final TreeNode<FormattedPost> node;
+  const PostNode({Key? key, required this.node}) : super(key: key);
 
-Future<List<TreeNode<FormattedPost>>> formatPosts(
-    int threadId, String tag) async {
-  //each formatted post will have a list of its parents
-  final formattedPosts = List<FormattedPost>.empty(growable: true);
-  final thread = await getThread(tag, threadId);
-  final opPost = thread!.posts!.first.num_;
-
-  for (var post in thread.posts!) {
-    var parents = getParents(post, opPost);
-
-    final formattedPost = FormattedPost(postInfo: post, parents: parents);
-    // for (var parent in parents){
-    //   final formattedPost = FormattedPost(postInfo: post, parents: parent);
-    // }
-    formattedPosts.add(formattedPost);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      //child: PostWidget(post: node.data),
+      child: Row(
+        children: [
+          node.hasNodes
+              ? IconButton(
+                  iconSize: 12,
+                  splashRadius: 16,
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints.tight(const Size(30, 30)),
+                  icon: Icon(node.expanded ? Icons.remove : Icons.add),
+                  onPressed: () {
+                    node.expanded = !node.expanded;
+                  },
+                )
+              : const SizedBox(
+                  width: 12,
+                ),
+          PostWidget(post: node.data),
+        ],
+      ),
+    );
   }
-
-  return createTreeModel(formattedPosts, formattedPosts.first.postInfo!.num_);
-}
-
-List<int> getParents(Post post, int? opPost) {
-  // extracts parent id from <a> tag of post comment.
-
-  //take post comment
-  final postCommentHtml = html.parse(post.comment);
-  // find <a> tags which contains data-num attribute
-  var aTags = postCommentHtml.getElementsByTagName("a");
-  final parents = List<int>.empty(growable: true);
-  for (var aTag in aTags) {
-    final keys = aTag.attributes.keys;
-    final values = aTag.attributes.values;
-    var attrMap = {}; // map
-    for (int i = 0; i < keys.length; i++) {
-      // create key-value pairs for attributes
-      attrMap[keys.elementAt(i)] = values.elementAt(i);
-    }
-    //take data-num attribute, it points to a parent post
-    if (attrMap.containsKey('data-num')) {
-      var parent = int.parse(attrMap['data-num']);
-      parents.add(parent);
-    }
-  }
-  return parents;
-}
-
-List<TreeNode<FormattedPost>> createTreeModel(
-    List<FormattedPost> posts, int? opPost) {
-  //final mainNode = TreeNode<FormattedPost>(data: FormattedPost(), id: 0);
-
-  // List of posts which doesn't have parents
-  final roots = List<TreeNode<FormattedPost>>.empty(growable: true);
-  for (var post in posts) {
-    if (post.parents!.isEmpty || post.parents!.contains(opPost)) {
-      //var node = TreeNode<FormattedPost>(data: post, id: post.postInfo!.num_);
-      var node = TreeNode<FormattedPost>(
-          data: post,
-          id: post.postInfo!.num_,
-          children: post.postInfo?.num_ != opPost
-              ? attachChilds(post.postInfo!.num_, posts)
-              : [],
-          expanded: true);
-      // if (post.postInfo?.num_ != opPost) {
-      //   node = attachChilds(node, posts);
-      // }
-      //mainNode.children?.add(node);
-      roots.add(node);
-    }
-  }
-  return roots;
-}
-
-List<TreeNode<FormattedPost>> attachChilds(int? id, List<FormattedPost> posts) {
-  var childrenToAdd = <TreeNode<FormattedPost>>[];
-  // find all posts that are replying to this one
-  Iterable<FormattedPost> childsFound =
-      posts.where((post) => post.parents?.contains(id) ?? false);
-  for (var post in childsFound) {
-    // add replies to them too
-    childrenToAdd.add(TreeNode(
-        data: post,
-        children: attachChilds(post.postInfo!.num_, posts),
-        expanded: true));
-  }
-  return childrenToAdd;
 }
 
 class PostWidget extends StatelessWidget {
   // widget represents post
-  final FormattedPost? post;
-  const PostWidget({super.key, this.post});
+  final FormattedPost post;
+  const PostWidget({super.key, required this.post});
 
   @override
   Widget build(BuildContext context) {
+    //return Text(post!.postInfo!.num_.toString());
     return Expanded(
       child: Card(
         child: Padding(
@@ -187,27 +110,28 @@ class PostWidget extends StatelessWidget {
               const Divider(
                 thickness: 1,
               ),
-              ImagesPreview(files: post!.postInfo!.files),
-              Html(
-                data: post!.postInfo!.comment,
-                style: {
-                  '#': Style(
-                      //margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-                      )
-                },
+              ImagesPreview(files: post.postInfo!.files),
+              ExcludeSemantics(
+                // Wrapped in ExcludeSemantics because of AssertError exception in debug mode
+                child: Html(
+                  data: post.postInfo!.comment,
+                  style: {
+                    '#': Style(
+                        //margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        )
+                  },
+                ),
               )
             ],
           ),
         ),
       ),
     );
-    //return Text(post!.postInfo!.num_.toString());
   }
 }
 
 class PostHeader extends StatelessWidget {
   const PostHeader({Key? key, required this.post}) : super(key: key);
-
   final FormattedPost? post;
   @override
   Widget build(BuildContext context) {
