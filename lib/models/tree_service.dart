6d7723dad1a7997cbe52c9d2afefcd70388a1adf
radeonovich <radeonovich1@yandex.ru>
2023-02-28 20:07:52 +0300

    refactor

--- lib/models/tree_service.dart
+++ lib/models/tree_service.dart
@@ -12,16 +12,16 @@ Future<ThreadContainer> getThreadContainer(int threadId, String tag,
 
   if (threadContainer.posts != null) {
     for (var post in threadContainer.posts!) {
-      post.parents = getPostParents(post);
+      post.parents = _getPostParents(post);
     }
-    threadContainer.roots = createTreeModel(
+    threadContainer.roots = _createTreeModel(
         threadContainer.posts!, threadContainer.threadInfo.opPostId);
   }
   return threadContainer;
 }
 
 /// Extracts parent id from <a> tag of post comment.
-List<int> getPostParents(Post post) {
+List<int> _getPostParents(Post post) {
   //take post comment
   final postCommentHtml = html.parse(post.comment);
   // find <a> tags which contains data-num attribute
@@ -45,18 +45,18 @@ List<int> getPostParents(Post post) {
 }
 
 /// Creates a list of root trees and connects childs to each.
-List<TreeNode<Post>> createTreeModel(List<Post> posts, int? opPost) {
+List<TreeNode<Post>> _createTreeModel(List<Post> posts, int? opPost) {
   // List of posts which doesn't have parents
   final roots = List<TreeNode<Post>>.empty(growable: true);
   for (var post in posts) {
     if (post.parents.isEmpty ||
         post.parents.contains(opPost) ||
-        hasExternalReferences(posts, post.parents)) {
+        _hasExternalReferences(posts, post.parents)) {
       // find posts which are replies to the OP-post
       TreeNode<Post> node = TreeNode<Post>(
           data: post,
           id: post.id,
-          children: post.id != opPost ? attachChilds(post.id, posts) : [],
+          children: post.id != opPost ? _attachChilds(post.id, posts) : [],
           expanded: true);
       roots.add(node);
     }
@@ -65,14 +65,14 @@ List<TreeNode<Post>> createTreeModel(List<Post> posts, int? opPost) {
 }
 
 /// Called recursively to connect post childs.
-List<TreeNode<Post>> attachChilds(int? id, List<Post> posts) {
+List<TreeNode<Post>> _attachChilds(int? id, List<Post> posts) {
   var childrenToAdd = <TreeNode<Post>>[];
   // find all posts that are replying to this one
   Iterable<Post> childsFound = posts.where((post) => post.parents.contains(id));
   for (var post in childsFound) {
     // add replies to them too
     childrenToAdd.add(TreeNode(
-        data: post, children: attachChilds(post.id, posts), expanded: true));
+        data: post, children: _attachChilds(post.id, posts), expanded: true));
   }
   return childrenToAdd;
 }
@@ -106,7 +106,7 @@ Future<ThreadContainer> refreshThread(
 }
 
 /// Check if post has references to posts in other threads.
-bool hasExternalReferences(List<Post> posts, List<int> referenceIds) {
+bool _hasExternalReferences(List<Post> posts, List<int> referenceIds) {
   for (var referenceId in referenceIds) {
     // if there are no posts with that id in current thread, then it is an external reference
     if (posts.where((post) => post.id == referenceId).isEmpty) {
@@ -124,7 +124,7 @@ TreeNode<Post>? findPost(List<TreeNode<Post>> roots, int id) {
       return roots[i];
     }
 
-    TreeNode<Post>? result = findPostInChildren(roots[i], id);
+    TreeNode<Post>? result = _findPostInChildren(roots[i], id);
     if (result == null) {
       continue;
     }
@@ -134,13 +134,13 @@ TreeNode<Post>? findPost(List<TreeNode<Post>> roots, int id) {
 }
 
 /// Called recursively.
-TreeNode<Post>? findPostInChildren(TreeNode<Post> node, int id) {
+TreeNode<Post>? _findPostInChildren(TreeNode<Post> node, int id) {
   // for (var child in node.children) doesn't work for some reason
   for (int i = 0; i < node.children.length; i++) {
     if (node.children[i].data.id == id) {
       return node.children[i];
     }
-    TreeNode<Post>? result = findPostInChildren(node.children[i], id);
+    TreeNode<Post>? result = _findPostInChildren(node.children[i], id);
     if (result == null) {
       continue;
     }
