6da9cc723cfc4738a4e6a0a66e7fd5157f8fbc67
radeonovich <radeonovich1@yandex.ru>
2023-04-08 22:56:11 +0300

    add link parser

---
+++ lib/services/search_bar_service.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:treechan/screens/tab_navigator.dart';
+
+class SearchBarService {
+  SearchBarService();
+
+  late String url;
+  late Uri parsedUrl;
+  late DrawerTab tab;
+  DrawerTab parseInput(String url) {
+    if (url == "") {
+      throw Exception("Empty url");
+    }
+    this.url = url;
+    parsedUrl = Uri.parse(url);
+
+    debugPrint("Got link: '$url' Parsed: ");
+    for (var segment in parsedUrl.pathSegments) {
+      debugPrint(segment);
+    }
+
+    if (parsedUrl.host.isNotEmpty) {
+      debugPrint("Host: ${parsedUrl.host}");
+    } else if (parsedUrl.pathSegments[0] == "2ch.hk") {
+      // fix if the user entered a link without the protocol
+      parsedUrl = Uri.parse("https://$url");
+      debugPrint("Fixed protocol: ");
+      for (var segment in parsedUrl.pathSegments) {
+        debugPrint(segment);
+      }
+    }
+    // TODO: add arch support
+    if (parsedUrl.pathSegments.isNotEmpty) {
+      tab = DrawerTab(
+          type: TabTypes.board,
+          tag: parsedUrl.pathSegments[0],
+          prevTab: boardListTab);
+      if (parsedUrl.pathSegments.length > 1) {
+        if (parsedUrl.pathSegments[1] == "res" &&
+            parsedUrl.pathSegments.length == 3) {
+          tab.type = TabTypes.thread;
+          // split is used to remove the .html extension
+          tab.id = int.parse(parsedUrl.pathSegments[2].split(".")[0]);
+        } else {
+          tab.type = TabTypes.thread;
+          tab.id = int.parse(parsedUrl.pathSegments[1].split(".")[0]);
+        }
+      }
+    }
+    debugPrint(tab.type.toString());
+    debugPrint(tab.tag);
+    debugPrint(tab.id.toString());
+    return tab;
+  }
+}
