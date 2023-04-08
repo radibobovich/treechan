14c330dea96c740ca38edc37b99ba37670d0071c
radeonovich <radeonovich1@yandex.ru>
2023-04-08 22:56:17 +0300

    add search bar

---
+++ lib/widgets/search_bar_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../services/search_bar_service.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar(
+      {super.key, required this.onOpen, required this.onCloseDrawer});
+  final Function onOpen;
+  final Function onCloseDrawer;
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final TextEditingController _controller = TextEditingController();
+  final SearchBarService searchBarService = SearchBarService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 40, 0, 10),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: "Ссылка или тег доски...",
+              ),
+            ),
+          ),
+          IconButton(
+              icon: const Icon(Icons.arrow_forward),
+              onPressed: () {
+                try {
+                  widget.onOpen(searchBarService.parseInput(_controller.text));
+                  widget.onCloseDrawer();
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Неверная ссылка"),
+                    ),
+                  );
+                }
+              })
+        ],
+      ),
+    );
+  }
+}
