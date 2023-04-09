e6fc6da80dfb6efff71cfe0db5dc8a24648512a5
radeonovich <radeonovich1@yandex.ru>
2023-04-09 14:35:34 +0300

    rename

--- lib/services/search_bar_service.dart
+++ lib/services/search_bar_service.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:treechan/screens/tab_navigator.dart';
 
 class SearchBarService {
-  SearchBarService();
+  SearchBarService({this.currentTab});
 
   late String url;
   late Uri parsedUrl;
-  late DrawerTab tab;
+  DrawerTab? currentTab;
+  late DrawerTab newTab;
   DrawerTab parseInput(String url) {
     List<String> allowedHosts = ["2ch.hk", "2ch.life"];
     if (url == "") {
@@ -35,25 +36,25 @@ class SearchBarService {
 
     // TODO: add arch support
     if (parsedUrl.pathSegments.isNotEmpty) {
-      tab = DrawerTab(
+      newTab = DrawerTab(
           type: TabTypes.board,
           tag: parsedUrl.pathSegments[0],
-          prevTab: boardListTab);
+          prevTab: currentTab ?? boardListTab);
       if (parsedUrl.pathSegments.length > 1) {
         if (parsedUrl.pathSegments[1] == "res" &&
             parsedUrl.pathSegments.length == 3) {
-          tab.type = TabTypes.thread;
+          newTab.type = TabTypes.thread;
           // split is used to remove the .html extension
-          tab.id = int.parse(parsedUrl.pathSegments[2].split(".")[0]);
+          newTab.id = int.parse(parsedUrl.pathSegments[2].split(".")[0]);
         } else {
-          tab.type = TabTypes.thread;
-          tab.id = int.parse(parsedUrl.pathSegments[1].split(".")[0]);
+          newTab.type = TabTypes.thread;
+          newTab.id = int.parse(parsedUrl.pathSegments[1].split(".")[0]);
         }
       }
     }
-    debugPrint(tab.type.toString());
-    debugPrint(tab.tag);
-    debugPrint(tab.id.toString());
-    return tab;
+    debugPrint(newTab.type.toString());
+    debugPrint(newTab.tag);
+    debugPrint(newTab.id.toString());
+    return newTab;
   }
 }
