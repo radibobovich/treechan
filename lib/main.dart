6c7b7905e006af96e3240684375e1217efed2016
radeonovich <radeonovich1@yandex.ru>
2023-04-17 19:43:27 +0300

    undo true

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ Future<void> initializePreferences() async {
   if (prefs.getString('androidDestinationType') == null) {
     await prefs.setString('androidDestinationType', 'directoryDownloads');
   }
-  if (prefs.getString('boardSortType') == null || true) {
+  if (prefs.getString('boardSortType') == null) {
     await prefs.setString('boardSortType', 'bump');
   }
 
