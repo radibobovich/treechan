0c9e5d938e2f6b99239567c05ef9f5c5ca57bc83
radeonovich <radeonovich1@yandex.ru>
2023-07-11 16:46:35 +0300

    improve tests

--- test/thread_service_test.dart
+++ test/thread_service_test.dart
@@ -1,6 +1,9 @@
+import 'package:flexible_tree_view/flexible_tree_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:treechan/domain/models/json/json.dart';
+import 'package:treechan/domain/models/tree.dart';
 import 'package:treechan/domain/services/thread_service.dart';
 
 late SharedPreferences prefs;
@@ -18,9 +21,9 @@ void main() async {
     });
   });
   test('ThreadService', () async {
-    final threadService = ThreadService(boardTag: 'pr', threadId: 1008826);
+    final threadService = ThreadService(boardTag: 'abu', threadId: 50074);
 
-    await threadService.getRoots();
+    List<TreeNode<Post>>? roots = await threadService.getRoots();
     final posts = threadService.getPosts;
     expect(posts, isNotNull);
     expect(posts, isNotEmpty);
@@ -28,5 +31,24 @@ void main() async {
     final threadInfo = threadService.getThreadInfo;
     expect(threadInfo.opPostId, posts![0].id);
     expect(threadInfo.maxNum, posts[posts.length - 1].id);
+
+    expect(roots!, isNotEmpty);
+
+    var post0 = Tree.findPost(roots, 50080);
+    expect(post0, isNotNull);
+    expect(post0!.children.length >= 3, isTrue);
+
+    // 55509 answers to 55504 and 55506
+    // check if tree matches these relationships
+    TreeNode<Post>? post1 = Tree.findPost(roots, 55504);
+    TreeNode<Post>? post2 = Tree.findPost(roots, 55506);
+    expect(post1, isNotNull);
+    expect(post2, isNotNull);
+
+    expect(post1!.children.where((element) => element.data.id == 55509),
+        isNotEmpty);
+
+    expect(post2!.children.where((element) => element.data.id == 55509),
+        isNotEmpty);
   });
 }
