c78722e4af31452c549114942da7c5b7d955e2b7
radeonovich <radeonovich1@yandex.ru>
2023-07-12 21:11:15 +0300

    remove unecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:treechan/config/themes.dart';
 import 'presentation/provider/tab_provider.dart';
