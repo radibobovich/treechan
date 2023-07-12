9453246d6a6f69925569558d61490ee9f796903c
radeonovich <radeonovich1@yandex.ru>
2023-07-12 21:12:32 +0300

    show response code to the user if failed

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -22,3 +22,14 @@ class BoardNotFoundException implements Exception {
     return message;
   }
 }
+
+class FailedResponseException implements Exception {
+  final String message;
+  final int statusCode;
+  FailedResponseException({required this.message, required this.statusCode});
+
+  @override
+  String toString() {
+    return message;
+  }
+}
