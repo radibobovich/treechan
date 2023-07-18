dec6fab3bfb40f92237be047830a3dd65641ee71
radeonovich <radeonovich1@yandex.ru>
2023-07-18 16:27:21 +0300

    refactor

--- lib/utils/fix_html_video.dart
+++ lib/utils/fix_html_video.dart
@@ -11,29 +11,30 @@ void fixHtmlVideo(dynamic item, {SortBy? sortType}) {
 }
 
 void _fixHtmlVideoInBoard(Thread thread) {
-  if (!thread.posts[0].comment.contains("<video")) return;
-  String video = thread.posts[0].comment.substring(
-      thread.posts[0].comment.indexOf("<video"),
-      thread.posts[0].comment.indexOf("</video>") + 8);
+  String comment = thread.posts.first.comment;
+  if (!comment.contains("<video")) return;
+  String video = comment.substring(
+      comment.indexOf("<video"), comment.indexOf("</video>") + 8);
   String src =
       video.substring(video.indexOf('src="') + 5, video.indexOf('"></video>'));
-  thread.posts[0].comment = thread.posts[0].comment
-      .substring(0, thread.posts[0].comment.indexOf("<video"));
-  thread.posts[0].files;
-  thread.posts[0].files?.add(File(
+  thread.posts.first.comment =
+      comment.substring(0, thread.posts.first.comment.indexOf("<video")) +
+          comment.substring(comment.indexOf("</video>") + 8);
+  thread.posts.first.files?.add(File(
       type: 10,
       path: "https://2ch.hk/$src",
       thumbnail: 'https://via.placeholder.com/640x360.png?text=No+Thumbnail'));
 }
 
 void _fixHtmlVideoInThread(Post post) {
-  if (!post.comment.contains("<video")) return;
-  String video = post.comment.substring(
-      post.comment.indexOf("<video"), post.comment.indexOf("</video>") + 8);
+  String comment = post.comment;
+  if (!comment.contains("<video")) return;
+  String video = comment.substring(
+      comment.indexOf("<video"), comment.indexOf("</video>") + 8);
   String src =
       video.substring(video.indexOf('src="') + 5, video.indexOf('"></video>'));
-  post.comment = post.comment.substring(0, post.comment.indexOf("<video"));
-  post.files;
+  post.comment = comment.substring(0, comment.indexOf("<video")) +
+      comment.substring(comment.indexOf("</video>") + 8);
   post.files?.add(File(
       type: 10,
       path: "https://2ch.hk/$src",
