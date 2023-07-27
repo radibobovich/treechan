c7d5b3b59e9d748e6c7bb2f9b172b3a92aabfe08
radeonovich <radeonovich1@yandex.ru>
2023-07-27 20:47:19 +0300

    move refresh logic to bloc

--- lib/presentation/bloc/thread_bloc.dart
+++ lib/presentation/bloc/thread_bloc.dart
@@ -1,18 +1,32 @@
+import 'dart:ui';
+
 import 'package:flexible_tree_view/flexible_tree_view.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:treechan/domain/models/tab.dart';
+import 'package:treechan/domain/services/scroll_service.dart';
 import 'package:treechan/exceptions.dart';
 
 import '../../domain/services/thread_service.dart';
 import '../../domain/models/json/json.dart';
+import '../provider/tab_provider.dart';
 
 class ThreadBloc extends Bloc<ThreadEvent, ThreadState> {
   late final ThreadService threadService;
   Key key;
-  // late List<TreeNode<Post>>? roots;
+  final DrawerTab tab;
+  final TabProvider provider;
+  final ScrollController scrollController = ScrollController();
+  late final ScrollService scrollService;
   late Root? threadInfo;
-  ThreadBloc({required this.threadService, required this.key})
+  ThreadBloc(
+      {required this.threadService,
+      required this.key,
+      required this.tab,
+      required this.provider})
       : super(ThreadInitialState()) {
+    scrollService = ScrollService(scrollController,
+        (window.physicalSize / window.devicePixelRatio).width);
     on<LoadThreadEvent>(
       (event, emit) async {
         try {
@@ -35,8 +49,29 @@ class ThreadBloc extends Bloc<ThreadEvent, ThreadState> {
     on<RefreshThreadEvent>(
       (event, emit) async {
         try {
+          //No need to preserve scroll position if the thread hasn't been loaded
+          // correctly. This check is created in case user presses refresh after
+          // failed thread loading.
+          int oldPostCount = threadService.getPosts.length;
+
+          if (oldPostCount > 0 && scrollController.offset != 0) {
+            scrollService.saveCurrentScrollInfo();
+          }
+          int lastIndex = threadService.getPosts.length - 1;
           await threadService.refresh();
           add(LoadThreadEvent());
+          provider.refreshRelatedBranches(tab, lastIndex);
+          int newPostCount = threadService.getPosts.length;
+
+          await Future.delayed(const Duration(milliseconds: 10));
+
+          if (oldPostCount > 0 &&
+              newPostCount > oldPostCount &&
+              scrollController.offset != 0) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              scrollService.updateScrollPosition();
+            });
+          }
         } on ThreadNotFoundException catch (e) {
           emit(ThreadErrorState(message: "404 - Тред умер", exception: e));
         } on NoConnectionException {
