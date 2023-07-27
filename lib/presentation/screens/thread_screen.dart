c67d394ef7e506cec1a2519ec3d0dfaeb2eb0b7c
radeonovich <radeonovich1@yandex.ru>
2023-07-27 20:47:19 +0300

    move refresh logic to bloc

--- lib/presentation/screens/thread_screen.dart
+++ lib/presentation/screens/thread_screen.dart
@@ -4,7 +4,6 @@ import 'package:flexible_tree_view/flexible_tree_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:treechan/presentation/widgets/thread/popup_menu_thread.dart';
-import 'dart:ui';
 
 import '../../exceptions.dart';
 import '../provider/tab_provider.dart';
@@ -14,7 +13,6 @@ import '../../main.dart';
 import '../../domain/models/json/json.dart';
 import '../bloc/thread_bloc.dart';
 
-import '../../domain/services/scroll_service.dart';
 import '../widgets/shared/go_back_widget.dart';
 import '../widgets/shared/no_connection_placeholder.dart';
 import '../widgets/thread/post_widget.dart';
@@ -35,19 +33,17 @@ class _ThreadScreenState extends State<ThreadScreen>
     with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
-  bool firstRun = true;
 
-  late ScrollController scrollController;
-  late ScrollService scrollService;
-  GlobalKey treeKey = GlobalKey();
+  // late ScrollController scrollController;
+  // late ScrollService scrollService;
 
-  @override
-  void initState() {
-    super.initState();
-    scrollController = ScrollController();
-    scrollService = ScrollService(scrollController,
-        (window.physicalSize / window.devicePixelRatio).width);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   scrollController = ScrollController();
+  //   scrollService = ScrollService(scrollController,
+  //       (window.physicalSize / window.devicePixelRatio).width);
+  // }
 
   @override
   void dispose() {
@@ -75,7 +71,8 @@ class _ThreadScreenState extends State<ThreadScreen>
           actions: <Widget>[
             IconButton(
                 onPressed: () async {
-                  _refreshThread();
+                  BlocProvider.of<ThreadBloc>(context)
+                      .add(RefreshThreadEvent());
                 },
                 icon: const Icon(Icons.refresh)),
             const PopupMenuThread()
@@ -94,20 +91,21 @@ class _ThreadScreenState extends State<ThreadScreen>
                 });
               }
               return FlexibleTreeView<Post>(
-                key: treeKey,
                 scrollable: prefs.getBool('2dscroll')!,
                 indent: !Platform.isWindows ? 16 : 24,
                 showLines: state.threadInfo!.showLines!,
-                scrollController: scrollController,
+                scrollController:
+                    BlocProvider.of<ThreadBloc>(context).scrollController,
                 nodes: state.roots!,
                 nodeWidth: MediaQuery.of(context).size.width / 1.5,
                 nodeItemBuilder: (context, node) {
                   return PostWidget(
-                    key: node.gKey,
+                    key: node.getGlobalKey(state.threadInfo!.opPostId!),
                     node: node,
                     roots: state.roots!,
                     currentTab: widget.currentTab,
-                    scrollService: scrollService,
+                    scrollService:
+                        BlocProvider.of<ThreadBloc>(context).scrollService,
                   );
                 },
               );
@@ -121,28 +119,5 @@ class _ThreadScreenState extends State<ThreadScreen>
             }
           },
         ));
-  }
-
-  Future<void> _refreshThread() async {
-    //No need to preserve scroll position if the thread hasn't been loaded
-    // correctly. This check is created in case user presses refresh after
-    // failed thread loading.
-    int oldPostCount =
-        BlocProvider.of<ThreadBloc>(context).threadService.getPosts.length;
-
-    if (oldPostCount > 0) {
-      scrollService.saveCurrentScrollInfo();
-    }
-    BlocProvider.of<ThreadBloc>(context).add(RefreshThreadEvent());
-    int newPostCount =
-        BlocProvider.of<ThreadBloc>(context).threadService.getPosts.length;
-
-    await Future.delayed(const Duration(milliseconds: 10));
-
-    if (oldPostCount > 0 && newPostCount > oldPostCount) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        scrollService.updateScrollPosition();
-      });
-    }
   }
 }
