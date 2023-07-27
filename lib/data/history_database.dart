227439fffa237233a68b2b9f56f2e8986f778c4a
radeonovich <radeonovich1@yandex.ru>
2023-07-27 20:43:28 +0300

    remove unnecessary import

--- lib/data/history_database.dart
+++ lib/data/history_database.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import '../utils/constants/enums.dart';
