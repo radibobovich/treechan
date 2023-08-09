116fe651f69e4b2a15787a50f50132e22379d502
radeonovich <radeonovich1@yandex.ru>
2023-08-10 01:49:42 +0300

    add board list search

--- lib/presentation/bloc/board_list_bloc.dart
+++ lib/presentation/bloc/board_list_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:treechan/domain/services/board_list_search_service.dart';
 import 'package:treechan/exceptions.dart';
 import '../../utils/constants/enums.dart';
 import '../../domain/services/board_list_service.dart';
@@ -10,6 +11,7 @@ class BoardListBloc extends Bloc<BoardListEvent, BoardListState> {
   late final BoardListService _boardListService;
   late List<Category> categories;
   late List<Board> favorites;
+  late BoardListSearchService searchService;
   foundation.Key key;
   bool allowReorder = false;
   BoardListBloc({required BoardListService boardListService, required this.key})
@@ -20,6 +22,7 @@ class BoardListBloc extends Bloc<BoardListEvent, BoardListState> {
         try {
           categories = await _boardListService.getCategories();
           favorites = _boardListService.getFavoriteBoards();
+
           emit(BoardListLoadedState(
               categories: categories,
               favorites: favorites,
@@ -62,6 +65,17 @@ class BoardListBloc extends Bloc<BoardListEvent, BoardListState> {
         }
       },
     );
+    on<SearchQueryChangedEvent>((event, emit) async {
+      try {
+        searchService =
+            BoardListSearchService(boards: _boardListService.boards);
+        emit(BoardListSearchState(
+            searchResult: searchService.search(event.query),
+            query: event.query));
+      } on Exception catch (e) {
+        emit(BoardListErrorState(message: e.toString(), exception: e));
+      }
+    });
   }
 }
 
@@ -78,6 +92,11 @@ class EditFavoritesEvent extends BoardListEvent {
   final FavoriteListAction action;
 }
 
+class SearchQueryChangedEvent extends BoardListEvent {
+  SearchQueryChangedEvent(this.query);
+  final String query;
+}
+
 abstract class BoardListState {}
 
 class BoardListInitialState extends BoardListState {}
@@ -90,6 +109,12 @@ class BoardListLoadedState extends BoardListState {
   List<Category> categories;
   List<Board> favorites;
   bool allowReorder;
+}
+
+class BoardListSearchState extends BoardListState {
+  BoardListSearchState({required this.searchResult, required this.query});
+  final List<Board> searchResult;
+  final String query;
 }
 
 class BoardListErrorState extends BoardListState {
