30d714e75ef21831acfc2da95c3b6926d1734d2b
radeonovich <radeonovich1@yandex.ru>
2023-08-15 01:08:02 +0300

    move file

--- lib/presentation/widgets/drawer/search_bar_widget.dart
+++ lib/presentation/widgets/drawer/search_bar_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../domain/services/search_bar_service.dart';
-import '../provider/page_provider.dart';
+import '../../../domain/services/search_bar_service.dart';
+import '../../provider/page_provider.dart';
 
 class SearchBar extends StatefulWidget {
   const SearchBar({
@@ -48,20 +48,9 @@ class _SearchBarState extends State<SearchBar> {
       context
           .read<PageProvider>()
           .addTab(searchBarService.parseInput(_controller.text));
-      // widget.onOpen(searchBarService.parseInput(_controller.text));
       widget.onCloseDrawer();
     } catch (e) {
-      // fix infinite duration
-
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   // TODO: make it appear above drawer
-      //   const SnackBar(
-      //     content: Text("Неверная ссылка"),
-      //     behavior: SnackBarBehavior.floating,
-      //     duration: Duration(seconds: 2),
-      //     elevation: 0,
-      //   ),
-      // );
+      // do nothing
     }
   }
 
