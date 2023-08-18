ab1a3078bbcb45b5b3aec1a37767b63ca8f29fc4
radeonovich <radeonovich1@yandex.ru>
2023-08-18 22:34:48 +0300

    inheritance, mixins experiments

--- lib/domain/models/tab.dart
+++ lib/domain/models/tab.dart
@@ -1,42 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:treechan/presentation/bloc/branch_bloc.dart';
+
+import '../../presentation/bloc/board_bloc.dart';
+import '../../presentation/bloc/board_list_bloc.dart';
+import '../../presentation/bloc/thread_bloc.dart';
+
 /// An initial tab for the drawer.
-DrawerTab boardListTab =
-    BoardListTab(name: "Доски", tag: "boards", prevTab: null);
+DrawerTab boardListTab = BoardListTab(name: "Доски");
 
 abstract class DrawerTab {
-  String tag;
   String? name;
-  final DrawerTab? prevTab;
-  DrawerTab({required this.tag, this.name, required this.prevTab});
+  DrawerTab({required this.name});
+  getBloc(BuildContext context);
+}
+
+mixin TagMixin {
+  late final String tag;
+  late DrawerTab prevTab;
+}
+
+/// Do not use this mixin without TagMixin.
+mixin IdMixin<T> {
+  late int id;
+  late DrawerTab prevTab;
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-
-    return other is DrawerTab && tag == other.tag;
+    return other is T &&
+        (this as dynamic).tag == (other as dynamic).tag &&
+        (this as dynamic).id == (other as dynamic).id;
   }
 
   @override
-  int get hashCode => tag.hashCode;
+  int get hashCode =>
+      (this as dynamic).tag.hashCode ^ (this as dynamic).id.hashCode;
 }
 
 class BoardListTab extends DrawerTab {
   BoardListTab({
-    required super.tag,
-    super.name,
-    required super.prevTab,
+    required super.name,
   });
+
+  @override
+  BoardListBloc getBloc(BuildContext context) {
+    return BlocProvider.of<BoardListBloc>(context);
+  }
 }
 
-class BoardTab extends DrawerTab {
+class BoardTab extends DrawerTab with TagMixin {
   bool isCatalog;
   String? query;
-  BoardTab({
-    required super.tag,
-    super.name,
-    required super.prevTab,
-    this.isCatalog = false,
-    this.query,
-  });
+  BoardTab(
+      {this.isCatalog = false,
+      this.query,
+      super.name,
+      required tag,
+      required prevTab}) {
+    this.tag = tag;
+    this.prevTab = prevTab;
+  }
+
+  @override
+  BoardBloc getBloc(BuildContext context) {
+    return BlocProvider.of<BoardBloc>(context);
+  }
 
   @override
   bool operator ==(Object other) {
@@ -49,14 +78,22 @@ class BoardTab extends DrawerTab {
   int get hashCode => tag.hashCode;
 }
 
-class ThreadTab extends DrawerTab {
-  final int id;
+class ThreadTab extends DrawerTab with TagMixin, IdMixin<ThreadTab> {
   ThreadTab({
-    required super.tag,
-    super.name,
-    required super.prevTab,
-    required this.id,
-  });
+    required super.name,
+    required tag,
+    required prevTab,
+    required id,
+  }) {
+    this.tag = tag;
+    this.prevTab = prevTab;
+    this.id = id;
+  }
+
+  @override
+  ThreadBloc getBloc(BuildContext context) {
+    return BlocProvider.of<ThreadBloc>(context);
+  }
 
   HistoryTab toHistoryTab() {
     return HistoryTab(
@@ -68,44 +105,52 @@ class ThreadTab extends DrawerTab {
     );
   }
 
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
+  // @override
+  // bool operator ==(Object other) {
+  //   if (identical(this, other)) return true;
 
-    return other is ThreadTab && tag == other.tag && id == other.id;
-  }
+  //   return other is ThreadTab && tag == other.tag && id == other.id;
+  // }
 
-  @override
-  int get hashCode => tag.hashCode ^ id.hashCode;
+  // @override
+  // int get hashCode => tag.hashCode ^ id.hashCode;
 }
 
-class BranchTab extends DrawerTab {
-  final int id;
+class BranchTab extends DrawerTab with TagMixin, IdMixin<BranchTab> {
   BranchTab({
-    required super.tag,
-    super.name,
-    required super.prevTab,
-    required this.id,
-  });
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is BranchTab && tag == other.tag && id == other.id;
+    required super.name,
+    required tag,
+    required prevTab,
+    required id,
+  }) {
+    this.tag = tag;
+    this.prevTab = prevTab;
+    this.id = id;
   }
 
   @override
-  int get hashCode => tag.hashCode ^ id.hashCode;
+  BranchBloc getBloc(BuildContext context) {
+    return BlocProvider.of<BranchBloc>(context);
+  }
+
+  // @override
+  // bool operator ==(Object other) {
+  //   if (identical(this, other)) return true;
+
+  //   return other is BranchTab && tag == other.tag && id == other.id;
+  // }
+
+  // @override
+  // int get hashCode => tag.hashCode ^ id.hashCode;
 }
 
-class HistoryTab extends DrawerTab {
-  final int id;
+class HistoryTab extends ThreadTab {
   DateTime timestamp;
   HistoryTab({
     required super.tag,
     required super.name,
-    required this.id,
     required super.prevTab,
+    required super.id,
     required this.timestamp,
   });
 
