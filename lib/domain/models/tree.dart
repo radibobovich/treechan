abf3a5b3dff9f76a7035e4c30f88a094f748511e
radibobovich <radeonovich1@yandex.ru>
2023-08-24 21:29:24 +0300

    move tree update to tree, kill isolate on timeout

--- lib/domain/models/tree.dart
+++ lib/domain/models/tree.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:treechan/exceptions.dart';
 
 import '../models/json/json.dart';
 
 import 'package:flexible_tree_view/flexible_tree_view.dart';
 import 'package:html/parser.dart' as html;
 
+/// Handles everything related to the tree building, updating and searching.
 class Tree {
   Tree({required this.posts, required this.threadInfo});
 
@@ -19,19 +23,25 @@ class Tree {
   /// or just a reply in the thread, or is an OP-post itself.
   /// Replies to OP-posts is not added as a children of the OP-post, but as a
   /// independent root.
-  List<TreeNode<Post>> _roots = [];
+  final List<TreeNode<Post>> _roots = [];
 
   List<TreeNode<Post>>? get getRoots => _roots;
 
-  Future<List<TreeNode<Post>>> getTree({bool skipPostsModify = false}) async {
+  Future<(List<TreeNode<Post>>, Map<int, List<TreeNode<Post>>>)> getTree(
+      {bool skipPostsModify = false}) async {
     final prefs = await SharedPreferences.getInstance();
     if (!skipPostsModify) {
       _findPostParents();
     }
     findChildren(posts);
+    var record = await compute(_createTreeModel, (posts, threadInfo, prefs))
+        .timeout(const Duration(seconds: 90))
+        .onError((error, stackTrace) {
+      debugPrint('TimeoutException:');
+      throw TreeBuilderTimeoutException('Building tree took too long.');
+    });
 
-    _roots = await compute(_createTreeModel, {posts, threadInfo, prefs});
-    return _roots;
+    return record;
   }
 
   /// Adds a list of parent posts to each post based on the comment html.
@@ -60,12 +70,69 @@ class Tree {
       post.parents = parents;
     }
     debugPrint('addPostParents() executed in ${stopwatch.elapsedMilliseconds}');
+    stopwatch.stop();
   }
 
-  /// Finds (first node by post id in the list of trees.
-  /// Worth noting that there might be multiple nodes containing same post
-  // TODO: make it search all occurences
+  Future<Map<int, List<TreeNode<Post>>>> attachNewRoots(
+      List<TreeNode<Post>> roots,
+      Map<int, List<TreeNode<Post>>> plainNodes,
+      List<Post> posts,
+      Root threadInfo) async {
+    final record = await getTree();
 
+    final List<TreeNode<Post>> newRoots = record.$1;
+    final Map<int, List<TreeNode<Post>>> newPlainNodes = record.$2;
+
+    if (newRoots.isEmpty) return {};
+    for (var newRoot in newRoots) {
+      for (var parentId in newRoot.data.parents) {
+        if (parentId != threadInfo.opPostId) {
+          // Find nodes to attach new tree to
+          final List<TreeNode<Post>> parentNodes = plainNodes[parentId] ?? [];
+
+          /// Attach new tree to each parent node
+          for (var parentNode in parentNodes) {
+            /// fix depth because here we add the same root in multiple places of the tree
+            /// Depth will be also increased by one in [addNode] method
+            /// Also override key to avoid duplicate GlobalObjectKeys
+            /// of [TreeNodeWidget] in flexible_tree_view package
+            parentNode.addNode(newRoot.copyWith(
+                key: parentNode.data.id.toString() + newRoot.data.id.toString(),
+                newKey: true)
+              ..depth = parentNode.depth);
+
+            /// find index of a child to add it to the post children list
+            int childIndex =
+                posts.indexWhere((child) => child.id == newRoot.data.id);
+            parentNode.data.children.add(childIndex);
+
+            /// find index of a parent post to update his children list too
+            int nodeIndex =
+                posts.indexWhere((element) => element.id == parentNode.data.id);
+
+            /// update children in [_posts]
+            posts[nodeIndex].children.add(childIndex);
+          }
+        } else {
+          roots.add(newRoot);
+        }
+      }
+
+      if (newRoot.data.parents.isEmpty) {
+        roots.add(newRoot);
+      }
+    }
+    return newPlainNodes;
+  }
+
+  /// Finds first node by post id in the list of trees.
+  ///
+  /// Use this only if you can't access [threadService._plainNodes] map.
+  /// Consider using the map if possible for O(1) complexity.
+  ///
+  /// Worth noting that there might be multiple nodes containing same post.
+  ///
+  /// Consider using [findAllNodes] instead if its important.
   static TreeNode<Post>? findNode(List<TreeNode<Post>> roots, int id) {
     final stopwatch = Stopwatch()..start();
     // for (var root in roots doesn't work for some reason)
@@ -74,38 +141,40 @@ class Tree {
         debugPrint(
             'findNode() executed in ${stopwatch.elapsedMicroseconds} microseconds');
         return roots[i];
+      } else if (roots[i].data.id < id) {
+        final TreeNode<Post>? result = findNode(roots[i].children, id);
+        if (result != null) return result;
       }
 
-      TreeNode<Post>? result = _findNodeInChildren(roots[i], id);
-      if (result == null) {
-        continue;
-      }
+      // TreeNode<Post>? result = _findNodeInChildren(roots[i], id);
+      // if (result == null) {
+      //   continue;
+      // }
+
       debugPrint(
           'findNode() executed in ${stopwatch.elapsedMicroseconds} microseconds');
-
-      return result;
+      stopwatch.stop();
     }
     return null;
   }
 
-  /// Called recursively.
-  /// TODO: make it search all occurences
-  
-  static TreeNode<Post>? _findNodeInChildren(TreeNode<Post> node, int id) {
-    // for (var child in node.children) doesn't work for some reason
-    for (int i = 0; i < node.children.length; i++) {
-      if (node.children[i].data.id == id) {
-        return node.children[i];
+  /// Finds all nodes by post id in the nodes of the list of trees.
+  ///
+  /// Use this only if you can't access [threadRepository.plainNodes] map.
+  /// Consider using the map if possible for O(1) complexity.
+  static List<TreeNode<Post>> findAllNodes(List<TreeNode<Post>> roots, int id) {
+    final List<TreeNode<Post>> results = [];
+    for (var root in roots) {
+      if (root.data.id == id) {
+        results.add(root);
+      } else if (root.data.id < id) {
+        results.addAll(findAllNodes(root.children, id));
       }
-      TreeNode<Post>? result = _findNodeInChildren(node.children[i], id);
-      if (result == null) {
-        continue;
-      }
-      return result;
     }
-    return null;
+    return results;
   }
 
+  /// Walks up the tree to find the root node.
   static TreeNode<Post> findRootNode(TreeNode<Post> node) {
     if (node.parent == null) {
       return node;
@@ -153,6 +222,7 @@ class Tree {
     }
     debugPrint(
         'performForEveryNodeInRoots() executed in ${stopwatch.elapsedMicroseconds} microseconds');
+    stopwatch.stop();
   }
 
   /// Performs operation with every node in this tree.
@@ -168,18 +238,21 @@ class Tree {
   }
 }
 
-/// Creates list of comment roots.
+/// Creates list of comment roots and map all nodes by post id.
 /// This is a heavy function and defined outside the class to use in an isolate.
-Future<List<TreeNode<Post>>> _createTreeModel(Set data) async {
-  List<Post> posts = data.elementAt(0);
-  Root threadInfo = data.elementAt(1);
-  SharedPreferences prefs = data.elementAt(2);
+Future<(List<TreeNode<Post>>, Map<int, List<TreeNode<Post>>>)> _createTreeModel(
+    (List<Post>, Root, SharedPreferences) record) async {
+  List<Post> posts = record.$1;
+  Root threadInfo = record.$2;
+  SharedPreferences prefs = record.$3;
 
-  final Set<int> postIds = posts.map((post) => post.id).toSet();
+  final Map<int, List<TreeNode<Post>>> plainNodes = {};
 
-  final stopwatch = Stopwatch()..start();
+  final Set<int> postsWithId = posts.map((post) => post.id).toSet();
 
   List<TreeNode<Post>>? roots = [];
+
+  final stopwatch = Stopwatch()..start();
 
   /// In case of thread refresh function [_isExternalReference()] works
   /// to determine if the post replies to the old posts in the thread
@@ -188,38 +261,71 @@ Future<List<TreeNode<Post>>> _createTreeModel(Set data) async {
   for (var post in posts) {
     if (post.parents.isEmpty ||
         post.parents.contains(threadInfo.opPostId) ||
-        _isExternalReference(postIds, post.parents)) {
+        _isExternalReference(postsWithId, post.parents)) {
       // find posts which are replies to the OP-post
       TreeNode<Post> node = TreeNode<Post>(
         expanded: !prefs.getBool("postsCollapsed")!,
         data: post,
         children: post.id != threadInfo.opPostId
-            ? _attachChildren(post, posts, prefs, 1)
+            ? _attachChildren(post, posts, prefs, plainNodes, stopwatch, 1).$1
             : [],
       );
       roots.add(node);
+
+      /// add to plainNodes too
+      if (plainNodes[post.id] == null) {
+        plainNodes[post.id] = [node];
+      } else {
+        plainNodes[post.id]!.add(node);
+      }
     }
   }
   debugPrint('createTreeModel() executed in ${stopwatch.elapsedMilliseconds}');
-  return roots;
+  stopwatch.stop();
+  return (roots, plainNodes);
 }
 
-/// Called recursively to connect post children.
-List<TreeNode<Post>> _attachChildren(
-    Post post, List<Post> posts, SharedPreferences prefs, int depth) {
+/// Called recursively to attach post children.
+(List<TreeNode<Post>>, Map<int, List<TreeNode<Post>>>) _attachChildren(
+    Post post,
+    List<Post> posts,
+    SharedPreferences prefs,
+    Map<int, List<TreeNode<Post>>> plainNodes,
+    Stopwatch stopwatch,
+    int depth) {
+  /// Kills isolate if it takes too long
+  if (stopwatch.elapsedMilliseconds > 91 * 1000) {
+    return (<TreeNode<Post>>[], <int, List<TreeNode<Post>>>{});
+  }
   var childrenToAdd = <TreeNode<Post>>[];
   // find all posts that are replying to this one
   List<int> children = post.children;
   for (var index in children) {
-    // if (index >= posts.length) continue;
     final child = posts[index];
-    // add replies to them too
-    childrenToAdd.add(TreeNode(
+
+    final node = TreeNode(
+
+        /// Make key unique to avoid GlobalKey and
+        /// GlobalObjectKey collisions due to the same root created at refresh
+        /// attached in multiple places of the tree.
+        key: UniqueKey().toString(),
         data: child,
-        children: _attachChildren(child, posts, prefs, depth + 1),
-        expanded: !prefs.getBool("postsCollapsed")!));
+        children: _attachChildren(
+                child, posts, prefs, plainNodes, stopwatch, depth + 1)
+            .$1,
+        expanded: !prefs.getBool("postsCollapsed")!);
+    childrenToAdd.add(node);
+
+    /// add to plainNodes too
+    if (plainNodes[node.data.id] == null) {
+      plainNodes[node.data.id] = [node];
+    } else {
+      if (!plainNodes[node.data.id]!.contains(node)) {
+        plainNodes[node.data.id]!.add(node);
+      }
+    }
   }
-  return childrenToAdd;
+  return (childrenToAdd, plainNodes);
 }
 
 List<Post> findChildren(List<Post> posts) {
