022cb71fe2019bf7afb519947f04a0edc240301a
radibobovich <radeonovich1@yandex.ru>
2023-09-04 17:22:19 +0300

    implement navbar settings menu

--- lib/presentation/widgets/board/popup_menu_board.dart
+++ lib/presentation/widgets/board/popup_menu_board.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:treechan/main.dart';
 import 'package:treechan/presentation/bloc/board_bloc.dart';
-import 'package:treechan/presentation/screens/hidden_threads_screen.dart';
 
 import '../../../domain/models/tab.dart';
 import '../../../utils/constants/enums.dart';
+import '../../provider/page_provider.dart';
 
+/// Called from AppBar button.
 class PopupMenuBoard extends StatelessWidget {
   final BoardTab currentTab;
   final Function onOpen;
@@ -22,93 +23,112 @@ class PopupMenuBoard extends StatelessWidget {
       icon: const Icon(Icons.more_vert),
       padding: EdgeInsets.zero,
       itemBuilder: (popupContext) {
-        if (BlocProvider.of<BoardBloc>(popupContext).boardRepository.sortType !=
-            SortBy.page) {
+        final bloc = BlocProvider.of<BoardBloc>(popupContext);
+        if (bloc.boardRepository.sortType != SortBy.page) {
           // catalog mode: can return to page mode, sort by time or bump and search
           return <PopupMenuEntry<dynamic>>[
-            getViewButton(popupContext),
-            getSortButton(popupContext),
-            getHiddenThreadsButton(context, onOpen),
+            getViewButton(popupContext, bloc),
+            getSortButton(popupContext, bloc),
+            getHiddenThreadsButton(popupContext, currentTab),
           ];
         } else {
           // page sort mode: can go to catalog
           return <PopupMenuEntry<dynamic>>[
-            getViewButton(popupContext),
-            getHiddenThreadsButton(context, onOpen),
+            getViewButton(popupContext, bloc),
+            getHiddenThreadsButton(context, currentTab),
           ];
         }
       },
     );
   }
+}
 
-  PopupMenuItem<dynamic> getViewButton(BuildContext context) {
-    if (BlocProvider.of<BoardBloc>(context).boardRepository.sortType ==
-        SortBy.page) {
-      return PopupMenuItem(
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-        child: const Text('Каталог'),
-        onTap: () {
-          BlocProvider.of<BoardBloc>(context).add(ChangeViewBoardEvent(null));
-          // setState(() {});
-        },
-      );
-    } else {
-      return PopupMenuItem(
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-        child: const Text('Страницы'),
-        onTap: () {
-          BlocProvider.of<BoardBloc>(context)
-              .add(ChangeViewBoardEvent(SortBy.page));
-        },
-      );
-    }
-  }
+/// Called from BottomNavigationBar button.
+void showPopupMenuBoard(
+    BuildContext context, BoardBloc bloc, BoardTab currentTab) {
+  final int tilesCount = bloc.boardRepository.sortType == SortBy.page ? 2 : 3;
+  final RelativeRect rect = RelativeRect.fromLTRB(
+      MediaQuery.of(context).size.width - 136, // width of popup menu
+      MediaQuery.of(context).size.height - tilesCount * 48 - 60,
+      // 48 is the height of one tile, 60 is approx. height of bottom bars
+      0,
+      0);
+  showMenu(
+    context: context,
+    position: rect,
+    items: bloc.boardRepository.sortType != SortBy.page
+        ? [
+            getViewButton(context, bloc),
+            getSortButton(context, bloc),
+            getHiddenThreadsButton(context, currentTab),
+          ]
+        : [
+            getViewButton(context, bloc),
+            getHiddenThreadsButton(context, currentTab),
+          ],
+    elevation: 8.0,
+  );
+}
 
-  PopupMenuItem<dynamic> getSortButton(BuildContext context) {
-    if (BlocProvider.of<BoardBloc>(context).boardRepository.sortType ==
-        SortBy.time) {
-      return PopupMenuItem(
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-        child: const Text('Сортировать по бампам'),
-        onTap: () {
-          prefs.setString('boardSortType', 'bump');
-          BlocProvider.of<BoardBloc>(context)
-              .add(ChangeViewBoardEvent(SortBy.bump));
-        },
-      );
-    } else {
-      return PopupMenuItem(
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-        child: const Text('Сортировать по дате'),
-        onTap: () {
-          prefs.setString('boardSortType', 'time');
-          BlocProvider.of<BoardBloc>(context)
-              .add(ChangeViewBoardEvent(SortBy.time));
-          BlocProvider.of<BoardBloc>(context).scrollToTop();
-        },
-      );
-    }
-  }
-
-  PopupMenuItem<dynamic> getHiddenThreadsButton(
-      BuildContext context, Function onOpen) {
+PopupMenuItem<dynamic> getViewButton(BuildContext context, BoardBloc bloc) {
+  if (bloc.boardRepository.sortType == SortBy.page) {
     return PopupMenuItem(
       padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-      child: const Text('Скрытые треды'),
+      child: const Text('Каталог'),
       onTap: () {
-        /// Use delay because handleTap() calls Navigator.pop() and interferes
-        /// with the push()
-        Future.delayed(
-            const Duration(milliseconds: 50),
-            () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HiddenThreadsScreen(
-                      tag: currentTab.tag,
-                      currentTab: currentTab,
-                      onOpen: onOpen),
-                )));
+        bloc.add(ChangeViewBoardEvent(null));
+        // setState(() {});
+      },
+    );
+  } else {
+    return PopupMenuItem(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: const Text('Страницы'),
+      onTap: () {
+        bloc.add(ChangeViewBoardEvent(SortBy.page));
       },
     );
   }
+}
+
+PopupMenuItem<dynamic> getSortButton(BuildContext context, BoardBloc bloc) {
+  if (bloc.boardRepository.sortType == SortBy.time) {
+    return PopupMenuItem(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: const Text('Сортировать по бампам'),
+      onTap: () {
+        prefs.setString('boardSortType', 'bump');
+        bloc.add(ChangeViewBoardEvent(SortBy.bump));
+      },
+    );
+  } else {
+    return PopupMenuItem(
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: const Text('Сортировать по дате'),
+      onTap: () {
+        prefs.setString('boardSortType', 'time');
+        bloc.add(ChangeViewBoardEvent(SortBy.time));
+        bloc.scrollToTop();
+      },
+    );
+  }
+}
+
+PopupMenuItem<dynamic> getHiddenThreadsButton(
+    BuildContext context, BoardTab currentTab) {
+  return PopupMenuItem(
+    padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+    child: const Text('Скрытые треды'),
+    onTap: () {
+      /// Use delay because handleTap() calls Navigator.pop() and interferes
+      /// with the push()
+      Future.delayed(
+          const Duration(milliseconds: 50),
+          () => Navigator.pushNamed(context, '/hidden_threads', arguments: {
+                'currentTab': currentTab,
+                'onOpen': (ThreadTab tab) =>
+                    context.read<PageProvider>().addTab(tab)
+              }));
+    },
+  );
 }
