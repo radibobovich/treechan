86581b97124d544b6147f5b5c76dd47760d748ce
radibobovich <radeonovich1@yandex.ru>
2023-09-20 01:00:49 +0300

    disable feature

--- lib/domain/services/image_download_service.dart
+++ lib/domain/services/image_download_service.dart
@@ -1,49 +1,49 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:image_downloader/image_downloader.dart';
+// import 'package:image_downloader/image_downloader.dart';
 
-import '../../main.dart';
+// import '../../main.dart';
 
-class ImageDownloadService {
-  ImageDownloadService();
+// class ImageDownloadService {
+//   ImageDownloadService();
 
-  late String? url;
-  void setUrl({required String url}) {
-    this.url = url;
-  }
+//   late String? url;
+//   void setUrl({required String url}) {
+//     this.url = url;
+//   }
 
-  Future<void> downloadImage() async {
-    if (url == null) {
-      return;
-    }
-    try {
-      // Saved with this method.
-      String? imageId;
-      if (Platform.isAndroid) {
-        imageId = await ImageDownloader.downloadImage(url!,
-            destination: _getDestinationType());
-      } else {
-        return;
-      }
+//   Future<void> downloadImage() async {
+//     if (url == null) {
+//       return;
+//     }
+//     try {
+//       // Saved with this method.
+//       String? imageId;
+//       if (Platform.isAndroid) {
+//         imageId = await ImageDownloader.downloadImage(url!,
+//             destination: _getDestinationType());
+//       } else {
+//         return;
+//       }
 
-      if (imageId == null) {
-        return;
-      }
-    } catch (e) {
-      throw Exception(e.toString());
-    }
-  }
-}
+//       if (imageId == null) {
+//         return;
+//       }
+//     } catch (e) {
+//       throw Exception(e.toString());
+//     }
+//   }
+// }
 
-AndroidDestinationType _getDestinationType() {
-  String androidDestinationType = prefs.getString('androidDestinationType')!;
-  if (androidDestinationType == 'directoryDownloads') {
-    return AndroidDestinationType.directoryDownloads;
-  } else if (androidDestinationType == 'directoryDCIM') {
-    return AndroidDestinationType.directoryDCIM;
-  } else if (androidDestinationType == 'directoryPictures') {
-    return AndroidDestinationType.directoryPictures;
-  } else {
-    return AndroidDestinationType.directoryMovies;
-  }
-}
+// AndroidDestinationType _getDestinationType() {
+//   String androidDestinationType = prefs.getString('androidDestinationType')!;
+//   if (androidDestinationType == 'directoryDownloads') {
+//     return AndroidDestinationType.directoryDownloads;
+//   } else if (androidDestinationType == 'directoryDCIM') {
+//     return AndroidDestinationType.directoryDCIM;
+//   } else if (androidDestinationType == 'directoryPictures') {
+//     return AndroidDestinationType.directoryPictures;
+//   } else {
+//     return AndroidDestinationType.directoryMovies;
+//   }
+// }
