84a5dba9e73fabf14ffbeaa1c20be99eb7cd964a
radibobovich <radeonovich1@yandex.ru>
2023-09-20 01:00:22 +0300

    add autorefresh funcs, move variables to the repo

--- lib/presentation/bloc/tracker_cubit.dart
+++ lib/presentation/bloc/tracker_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:treechan/domain/repositories/tracker_repository.dart';
 
 import '../../domain/models/tracked_item.dart';
@@ -8,14 +9,16 @@ import '../../domain/models/tracked_item.dart';
 class TrackerCubit extends Cubit<TrackerState> {
   final TrackerRepository trackerRepository;
   TrackerCubit({required this.trackerRepository})
-      : super(TrackerInitialState());
+      : super(TrackerInitialState()) {
+    trackerRepository.autoRefreshNotifier.stream.listen((item) {
+      refreshItem(item);
+    });
+  }
 
-  late List<TrackedThread> threads;
-  late List<TrackedBranch> branches;
   void loadTracker() async {
     try {
-      threads = await trackerRepository.getTrackedThreads();
-      branches = await trackerRepository.getTrackedBranches();
+      final threads = await trackerRepository.getTrackedThreads();
+      final branches = await trackerRepository.getTrackedBranches();
       emit(TrackerLoadedState(threads: threads, branches: branches));
     } catch (e) {
       emit(TrackerErrorState(e.toString()));
@@ -24,6 +27,8 @@ class TrackerCubit extends Cubit<TrackerState> {
 
   void refreshAll() async {
     try {
+      final threads = trackerRepository.threads.toList();
+      final branches = trackerRepository.branches.toList();
       for (var item in [...threads, ...branches]) {
         await Future.delayed(const Duration(milliseconds: 1000));
         await refreshItem(item);
@@ -37,6 +42,8 @@ class TrackerCubit extends Cubit<TrackerState> {
 
   Future<void> refreshItem(TrackedItem item) async {
     try {
+      var threads = trackerRepository.threads;
+      var branches = trackerRepository.branches;
       emit(TrackerRefreshingState(
         threads: threads,
         branches: branches,
@@ -69,6 +76,29 @@ class TrackerCubit extends Cubit<TrackerState> {
     } catch (e) {
       emit(TrackerErrorState(e.toString()));
     }
+  }
+
+  Future<void> toggleAutoRefresh() async {
+    final prefs = await SharedPreferences.getInstance();
+    final autoRefresh = prefs.getBool('trackerAutoRefresh') ?? true;
+    await prefs.setBool('trackerAutoRefresh', !autoRefresh);
+  }
+
+  Future<bool> getAutoRefreshStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final autoRefresh = prefs.getBool('trackerAutoRefresh') ?? true;
+    return autoRefresh;
+  }
+
+  Future<int> getRefreshInterval() async {
+    final prefs = await SharedPreferences.getInstance();
+    final interval = prefs.getInt('refreshInterval') ?? 60;
+    return interval;
+  }
+
+  void setRefreshInterval(int interval) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('refreshInterval', interval);
   }
 }
 
