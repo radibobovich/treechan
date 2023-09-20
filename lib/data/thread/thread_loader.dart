aacb29c52a1dd1527c65b94d896fd115b2363743
radibobovich <radeonovich1@yandex.ru>
2023-09-21 01:18:24 +0300

    implement injectable

--- lib/data/thread/thread_loader.dart
+++ lib/data/thread/thread_loader.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
+import 'package:mockito/mockito.dart';
+import 'package:treechan/di/injection.dart';
 import 'package:treechan/domain/models/json/json.dart';
 
 import 'response_handler.dart';
@@ -15,9 +20,11 @@ abstract class IThreadLoader {
   });
 }
 
+@Injectable(as: IThreadLoader, env: [Env.prod])
 class ThreadLoader implements IThreadLoader {
-  const ThreadLoader(this.responseHandler);
-  final IResponseHandler responseHandler;
+  ThreadLoader();
+
+  final IResponseHandler responseHandler = getIt<IResponseHandler>();
   @override
   Future<http.Response> _getThreadResponse(
     String boardTag,
@@ -28,6 +35,34 @@ class ThreadLoader implements IThreadLoader {
 
     return await responseHandler.getResponse(
         url: url, boardTag: boardTag, threadId: threadId);
+  }
+
+  @override
+  Future<List<Post>> getPosts(
+      {required String boardTag, required int threadId}) async {
+    final http.Response response = await _getThreadResponse(boardTag, threadId);
+
+    Root decodedResponse = Root.fromJson(jsonDecode(response.body));
+    // threadInfo = decodedResponse;
+    return decodedResponse.threads!.first.posts;
+  }
+}
+
+@Injectable(as: IThreadLoader, env: [Env.test, Env.dev])
+class MockThreadLoader extends Mock implements IThreadLoader {
+  MockThreadLoader({@factoryParam required this.assetPath});
+  final String assetPath;
+  @override
+  Future<http.Response> _getThreadResponse(
+    String boardTag,
+    int threadId,
+  ) async {
+    String jsonString = await rootBundle.loadString(assetPath);
+    http.Response response = http.Response(jsonString, 200, headers: {
+      HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+    });
+
+    return response;
   }
 
   @override
--- lib/data/thread/thread_refresher.dart
+++ lib/data/thread/thread_refresher.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
+import 'package:mockito/mockito.dart';
 import 'package:treechan/data/thread/response_handler.dart';
+import 'package:treechan/di/injection.dart';
 import 'package:treechan/domain/models/json/json.dart';
 
 abstract class IThreadRefresher {
@@ -16,9 +21,10 @@ abstract class IThreadRefresher {
       required int lastPostId});
 }
 
+@Injectable(as: IThreadRefresher, env: [Env.prod])
 class ThreadRefresher implements IThreadRefresher {
-  const ThreadRefresher(this.responseHandler);
-  final IResponseHandler responseHandler;
+  ThreadRefresher();
+  final IResponseHandler responseHandler = getIt<IResponseHandler>();
   @override
   Future<http.Response> _getRefreshResponse(
     String boardTag,
@@ -38,6 +44,43 @@ class ThreadRefresher implements IThreadRefresher {
     required int threadId,
     required int lastPostId,
   }) async {
+    final http.Response response =
+        await _getRefreshResponse(boardTag, threadId, lastPostId);
+
+    debugPrint('Thread $boardTag/$threadId refreshed');
+    return postListFromJson(jsonDecode(response.body)["posts"]);
+  }
+}
+
+@Injectable(as: IThreadRefresher, env: [Env.test, Env.dev])
+class MockThreadRefresher extends Mock implements IThreadRefresher {
+  MockThreadRefresher({@factoryParam required this.assetPaths});
+  final List<String> assetPaths;
+  int refreshCount = 0;
+  @override
+  Future<http.Response> _getRefreshResponse(
+    String boardTag,
+    int threadId,
+    int lastPostId,
+  ) async {
+    String jsonString = await rootBundle.loadString(assetPaths[refreshCount++]);
+    http.Response response = http.Response(jsonString, 200, headers: {
+      HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+    });
+
+    return response;
+  }
+
+  @override
+  Future<List<Post>> getNewPosts({
+    required String boardTag,
+    required int threadId,
+    required int lastPostId,
+  }) async {
+    if (refreshCount >= assetPaths.length) {
+      debugPrint('No more refreshes left in refresh asset list.');
+      return [];
+    }
     final http.Response response =
         await _getRefreshResponse(boardTag, threadId, lastPostId);
 
