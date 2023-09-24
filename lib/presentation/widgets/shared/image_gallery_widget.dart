5143ee28614d047100a1fa9b4635df5dd1c21be7
radibobovich <radeonovich1@yandex.ru>
2023-09-24 03:58:32 +0300

    add custom loading, double tap and cache

--- lib/presentation/widgets/shared/image_gallery_widget.dart
+++ lib/presentation/widgets/shared/image_gallery_widget.dart
@@ -2,17 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:extended_image/extended_image.dart';
 import 'dart:math';
 
+typedef DoubleClickAnimationListener = void Function();
+
 /// A gallery that opens when user tap on image preview.
-class SwipeGallery extends StatelessWidget {
+class SwipeGallery extends StatefulWidget {
   const SwipeGallery(
       {super.key,
       required this.imageLinks,
+      required this.previewLinks,
       required this.pageController,
       required this.onDownloadImage});
 
   final List<String> imageLinks;
+  final List<String> previewLinks;
   final ExtendedPageController pageController;
   final Function onDownloadImage;
+
+  @override
+  State<SwipeGallery> createState() => _SwipeGalleryState();
+}
+
+class _SwipeGalleryState extends State<SwipeGallery>
+    with TickerProviderStateMixin {
+  late AnimationController _doubleClickAnimationController;
+  Animation<double>? _doubleClickAnimation;
+  late DoubleClickAnimationListener _doubleClickAnimationListener;
+  List<double> doubleTapScales = <double>[1.0, 2.0];
+
+  @override
+  void initState() {
+    super.initState();
+    _doubleClickAnimationController = AnimationController(
+        duration: const Duration(milliseconds: 150), vsync: this);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _preloadImage(1);
+  }
+
+  @override
+  void dispose() {
+    _doubleClickAnimationController.dispose();
+    _doubleClickAnimation?.removeListener(_doubleClickAnimationListener);
+    super.dispose();
+  }
+
+  final List<int> _cachedIndexes = <int>[];
+
+  void _preloadImage(int index) {
+    if (_cachedIndexes.contains(index)) {
+      return;
+    }
+    if (0 <= index && index < widget.imageLinks.length) {
+      final String url = widget.imageLinks[index];
+
+      precacheImage(ExtendedNetworkImageProvider(url, cache: true), context);
+
+      _cachedIndexes.add(index);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ExtendedImageSlidePage(
@@ -24,10 +75,14 @@ class SwipeGallery extends StatelessWidget {
         return Colors.black.withOpacity(min(1.0, max(1.0 - opacity, 0.0)));
       },
       child: ExtendedImageGesturePageView.builder(
+        onPageChanged: (int page) {
+          _preloadImage(page - 1);
+          _preloadImage(page + 1);
+        },
         itemBuilder: (context, index) {
-          onDownloadImage(imageLinks[index], index);
+          widget.onDownloadImage(widget.imageLinks[index], index);
           return ExtendedImage.network(
-            imageLinks[index],
+            widget.imageLinks[index],
             fit: BoxFit.contain,
             enableSlideOutPage: true,
             mode: ExtendedImageMode.gesture,
@@ -44,10 +99,76 @@ class SwipeGallery extends StatelessWidget {
                 initialAlignment: InitialAlignment.center,
               );
             },
+            loadStateChanged: (state) {
+              if (state.extendedImageLoadState == LoadState.loading) {
+                final ImageChunkEvent? loadingProgress = state.loadingProgress;
+                final double? progress =
+                    loadingProgress?.expectedTotalBytes != null
+                        ? loadingProgress!.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes!
+                        : null;
+                return Stack(
+                  alignment: AlignmentDirectional.center,
+                  fit: StackFit.expand,
+                  children: [
+                    ExtendedImage.network(
+                      mode: ExtendedImageMode.none,
+                      widget.previewLinks[index],
+                      fit: BoxFit.contain,
+                      loadStateChanged: (state) {
+                        if (state.extendedImageLoadState ==
+                            LoadState.completed) {
+                          return null;
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    ),
+                    Center(child: CircularProgressIndicator(value: progress)),
+                  ],
+                );
+              }
+              return null;
+            },
+            onDoubleTap: (state) {
+              final Offset? pointerDownPosition = state.pointerDownPosition;
+              final double? begin = state.gestureDetails!.totalScale;
+              double end;
+
+              //remove old
+              _doubleClickAnimation
+                  ?.removeListener(_doubleClickAnimationListener);
+
+              //stop pre
+              _doubleClickAnimationController.stop();
+
+              //reset to use
+              _doubleClickAnimationController.reset();
+
+              if (begin == doubleTapScales[0]) {
+                end = doubleTapScales[1];
+              } else {
+                end = doubleTapScales[0];
+              }
+
+              _doubleClickAnimationListener = () {
+                //print(_animation.value);
+                state.handleDoubleTap(
+                    scale: _doubleClickAnimation!.value,
+                    doubleTapPosition: pointerDownPosition);
+              };
+              _doubleClickAnimation = _doubleClickAnimationController
+                  .drive(Tween<double>(begin: begin, end: end));
+
+              _doubleClickAnimation!.addListener(_doubleClickAnimationListener);
+
+              _doubleClickAnimationController.forward();
+            },
           );
         },
-        itemCount: imageLinks.length,
-        controller: pageController,
+        itemCount: widget.imageLinks.length,
+        controller: widget.pageController,
         scrollDirection: Axis.horizontal,
       ),
     );
