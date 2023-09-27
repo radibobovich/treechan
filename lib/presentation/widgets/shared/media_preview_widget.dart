6e3dabee4dd5638dcb62d29f58f34e943c0957c7
radibobovich <radeonovich1@yandex.ru>
2023-09-27 03:19:45 +0300

    refactor, mock, custom height

--- lib/presentation/widgets/shared/media_preview_widget.dart
+++ lib/presentation/widgets/shared/media_preview_widget.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:treechan/di/injection.dart';
 import 'package:treechan/domain/services/image_download_service.dart';
+import 'package:treechan/utils/constants/dev.dart';
 
 import '../../../domain/models/json/json.dart';
 import 'package:extended_image/extended_image.dart';
@@ -9,11 +13,17 @@ import 'video_player_widget.dart';
 
 /// Scrollable horizontal row with image previews.
 class MediaPreview extends StatelessWidget {
-  const MediaPreview({Key? key, required this.files}) : super(key: key);
+  const MediaPreview({Key? key, required this.files, this.height = 140})
+      : super(key: key);
   final List<File>? files;
+  final double height;
   @override
   Widget build(BuildContext context) {
-    fixLinks(files);
+    if (files == null) return const SizedBox.shrink();
+    _fixLinks(files!);
+    if (env == Env.test || env == Env.dev) {
+      _mockLinks(files!);
+    }
     return Padding(
       padding:
           (files == null ? const EdgeInsets.all(0) : const EdgeInsets.all(8.0)),
@@ -21,17 +31,14 @@ class MediaPreview extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         physics: const ClampingScrollPhysics(),
         child: Row(
-          children: _getImages(files, context),
+          children: _getImages(files!, context, height: height),
         ),
       ),
     );
   }
 }
 
-void fixLinks(List<File>? files) {
-  if (files == null) {
-    return;
-  }
+void _fixLinks(List<File> files) {
   for (var file in files) {
     if (file.thumbnail != null && !file.thumbnail!.contains("http")) {
       file.thumbnail = "https://2ch.hk${file.thumbnail}";
@@ -42,15 +49,24 @@ void fixLinks(List<File>? files) {
   }
 }
 
+/// Used for testing purposes
+void _mockLinks(List<File> files) {
+  final List<String> mockImages = [
+    'https://2ch.hk/abu/thumb/42375/14433751460020s.jpg',
+    'https://2ch.hk/abu/thumb/52084/14760211194610s.jpg',
+    'https://2ch.hk/abu/thumb/50159/14713521951590s.jpg',
+    'https://2ch.hk/abu/thumb/39173/14229147201130s.jpg',
+  ];
+  for (var file in files) {
+    file.thumbnail = mockImages[Random().nextInt(mockImages.length)];
+  }
+}
+
 List<int> _galleryTypes = [0, 1, 2, 4, 9]; // none jpg png gif jpg respectively
 List<int> _videoTypes = [6, 10]; // webm mp4
 
-List<Widget> _getImages(List<File>? files, BuildContext context) {
-  if (files == null) {
-    // return empty image list
-    return List<Widget>.filled(1, const SizedBox.shrink());
-  }
-
+List<Widget> _getImages(List<File> files, BuildContext context,
+    {required double height}) {
   List<Widget> media = [];
   List<String> fullResLinks = [];
   List<String> previewLinks = [];
@@ -66,39 +82,38 @@ List<Widget> _getImages(List<File>? files, BuildContext context) {
   }
   for (var file in files) {
     media.add(_MediaItemPreview(
-        imageLinks: fullResLinks,
-        previewLinks: previewLinks,
-        file: file,
-        context: context,
-        type: file.type!));
+      imageLinks: fullResLinks,
+      previewLinks: previewLinks,
+      file: file,
+      type: file.type!,
+      height: height,
+    ));
   }
   return media;
 }
 
 /// Represents one specific media item in a row.
 class _MediaItemPreview extends StatefulWidget {
-  const _MediaItemPreview(
-      {Key? key,
-      required this.imageLinks,
-      required this.previewLinks,
-      required this.file,
-      required this.context,
-      required this.type})
-      : super(key: key);
+  const _MediaItemPreview({
+    Key? key,
+    required this.imageLinks,
+    required this.previewLinks,
+    required this.file,
+    required this.type,
+    required this.height,
+  }) : super(key: key);
 
   final List<String> imageLinks;
   final List<String> previewLinks;
   final File file;
-  final BuildContext context;
   final int type;
+  final double height;
   @override
   State<_MediaItemPreview> createState() => _MediaItemPreviewState();
 }
 
 class _MediaItemPreviewState extends State<_MediaItemPreview>
     with SingleTickerProviderStateMixin {
-  // ImageDownloadService imageDownloadService = ImageDownloadService();
-
   late Widget thumbnail;
   bool isLoaded = true;
 
@@ -157,17 +172,6 @@ class _MediaItemPreviewState extends State<_MediaItemPreview>
                   onPressed: () {
                     downloadImage(
                         widget.imageLinks[pageController.page?.toInt() ?? 0]);
-                    // imageDownloadService.downloadImage();
-                    // fix infinite duration
-
-                    // ScaffoldMessenger.of(context).showSnackBar(
-                    //   const SnackBar(
-                    //     content: Text("Загружено"),
-                    //     behavior: SnackBarBehavior.floating,
-                    //     duration: Duration(seconds: 3),
-                    //     elevation: 0,
-                    //   ),
-                    // );
                   },
                   // add a notification here to show that the image is downloaded
                 )
@@ -178,9 +182,6 @@ class _MediaItemPreviewState extends State<_MediaItemPreview>
                     imageLinks: widget.imageLinks,
                     previewLinks: widget.previewLinks,
                     pageController: pageController,
-                    onDownloadImage: (imageUrl, fileName) {
-                      // imageDownloadService.setUrl(url: imageUrl);
-                    },
                   )
                 : VideoPlayer(file: widget.file)),
       ),
@@ -191,8 +192,8 @@ class _MediaItemPreviewState extends State<_MediaItemPreview>
     if (_galleryTypes.contains(type)) {
       return Image.network(
         widget.file.thumbnail!,
-        height: 140,
-        width: 140.0 *
+        height: widget.height,
+        width: widget.height *
             widget.file.width!.toDouble() /
             widget.file.height!.toDouble(),
         fit: BoxFit.fill,
