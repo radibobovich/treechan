26e2c3c7cc17f8604276f9ed901567c253f6cafd
radibobovich <radeonovich1@yandex.ru>
2023-10-07 01:59:14 +0300

    test archive loading

--- test/api_test/dvach_rest_client_test.dart
+++ test/api_test/dvach_rest_client_test.dart
@@ -66,36 +66,28 @@ main() {
       final client = DvachArchiveRestClient(Dio());
 
       ThreadArchiveResponseDvachApiModel model = await client.loadThread(
-          boardTag: 'a',
-          date: '2016-02-13',
-          threadId: 2656447) as ThreadArchiveResponseDvachApiModel;
+          boardTag: 'a', date: '2016-02-13', threadId: 2656447);
       expect(model.Board, 'a');
       expect(model.threads.first.posts, isNotEmpty);
     });
     test('Archived thread test (2018/01/01)', () async {
       final client = DvachArchiveRestClient(Dio());
       ThreadArchiveResponseDvachApiModel model = await client.loadThread(
-          boardTag: 'a',
-          date: '2018-01-01',
-          threadId: 4242071) as ThreadArchiveResponseDvachApiModel;
+          boardTag: 'a', date: '2018-01-01', threadId: 4242071);
       expect(model.Board, 'a');
       expect(model.threads.first.posts, isNotEmpty);
     });
     test('Archived thread test (2020/05/04)', () async {
       final client = DvachArchiveRestClient(Dio());
       ThreadArchiveResponseDvachApiModel model = await client.loadThread(
-          boardTag: 'a',
-          date: '2020-05-04',
-          threadId: 6751297) as ThreadArchiveResponseDvachApiModel;
+          boardTag: 'a', date: '2020-05-04', threadId: 6751297);
       expect(model.Board, 'a');
       expect(model.threads.first.posts, isNotEmpty);
     });
     test('Archived thread test (2021/04/04)', () async {
       final client = DvachArchiveRestClient(Dio());
       ThreadArchiveResponseDvachApiModel model = await client.loadThread(
-          boardTag: 'a',
-          date: '2021-04-04',
-          threadId: 7183005) as ThreadArchiveResponseDvachApiModel;
+          boardTag: 'a', date: '2021-04-04', threadId: 7183005);
       expect(model.Board, 'a');
       expect(model.threads.first.posts, isNotEmpty);
     });
