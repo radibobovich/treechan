48d02b6317f5057daf79ce3729ebccca8ebf62df
radibobovich <radeonovich1@yandex.ru>
2023-10-08 17:05:36 +0300

    generator update

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -9,15 +9,14 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i10;
+import 'package:dio/dio.dart' as _i9;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:treechan/data/board_fetcher.dart' as _i3;
 import 'package:treechan/data/local/history_database.dart' as _i5;
-import 'package:treechan/data/rest/rest_client.dart' as _i9;
-import 'package:treechan/data/thread/thread_loader.dart' as _i7;
-import 'package:treechan/data/thread/thread_refresher.dart' as _i8;
-import 'package:treechan/deprecated/response_handler.dart' as _i6;
+import 'package:treechan/data/rest/rest_client.dart' as _i8;
+import 'package:treechan/data/thread/thread_loader.dart' as _i6;
+import 'package:treechan/data/thread/thread_refresher.dart' as _i7;
 import 'package:treechan/utils/constants/enums.dart' as _i4;
 
 const String _prod = 'prod';
@@ -68,31 +67,23 @@ _i1.GetIt init(
       _prod,
     },
   );
-  gh.lazySingleton<_i6.IResponseHandler>(
-    () => _i6.ResponseHandler(),
-    registerFor: {
-      _test,
-      _dev,
-      _prod,
-    },
-  );
-  gh.factoryParam<_i7.IThreadRemoteLoader, _i4.Imageboard, String>(
+  gh.factoryParam<_i6.IThreadRemoteLoader, _i4.Imageboard, String>(
     (
       imageboard,
       assetPath,
     ) =>
-        _i7.ThreadRemoteLoader(
+        _i6.ThreadRemoteLoader(
       imageboard: imageboard,
       assetPath: assetPath,
     ),
     registerFor: {_prod},
   );
-  gh.factoryParam<_i7.IThreadRemoteLoader, _i4.Imageboard, String>(
+  gh.factoryParam<_i6.IThreadRemoteLoader, _i4.Imageboard, String>(
     (
       imageboard,
       assetPath,
     ) =>
-        _i7.MockThreadRemoteLoader(
+        _i6.MockThreadRemoteLoader(
       imageboard: imageboard,
       assetPath: assetPath,
     ),
@@ -101,23 +92,23 @@ _i1.GetIt init(
       _dev,
     },
   );
-  gh.factoryParam<_i8.IThreadRemoteRefresher, _i4.Imageboard, dynamic>(
+  gh.factoryParam<_i7.IThreadRemoteRefresher, _i4.Imageboard, dynamic>(
     (
       imageboard,
       assetPaths,
     ) =>
-        _i8.ThreadRemoteRefresher(
+        _i7.ThreadRemoteRefresher(
       imageboard: imageboard,
       assetPaths: assetPaths,
     ),
     registerFor: {_prod},
   );
-  gh.factoryParam<_i8.IThreadRemoteRefresher, _i4.Imageboard, List<String>>(
+  gh.factoryParam<_i7.IThreadRemoteRefresher, _i4.Imageboard, List<String>>(
     (
       imageboard,
       assetPaths,
     ) =>
-        _i8.MockThreadRemoteRefresher(
+        _i7.MockThreadRemoteRefresher(
       imageboard: imageboard,
       assetPaths: assetPaths,
     ),
@@ -126,12 +117,12 @@ _i1.GetIt init(
       _dev,
     },
   );
-  gh.factoryParam<_i9.RestClient, _i10.Dio, dynamic>(
+  gh.factoryParam<_i8.RestClient, _i9.Dio, dynamic>(
     (
       dio,
       _,
     ) =>
-        _i9.DvachRestClient(dio),
+        _i8.DvachRestClient(dio),
     instanceName: 'dvach',
     registerFor: {
       _test,
@@ -139,12 +130,12 @@ _i1.GetIt init(
       _prod,
     },
   );
-  gh.factoryParam<_i9.RestClient, _i10.Dio, dynamic>(
+  gh.factoryParam<_i8.RestClient, _i9.Dio, dynamic>(
     (
       dio,
       _,
     ) =>
-        _i9.DvachArchiveRestClient(dio),
+        _i8.DvachArchiveRestClient(dio),
     instanceName: 'dvachArchive',
     registerFor: {
       _test,
