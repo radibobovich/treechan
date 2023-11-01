2ce20b620acc45cff384328409cf5412ded0e857
radibobovich <radeonovich1@yandex.ru>
2023-11-01 14:43:14 +0300

    add more methods

--- lib/data/local/dao/filter_daos.dart
+++ lib/data/local/dao/filter_daos.dart
@@ -19,20 +19,19 @@ abstract class FilterDao {
   @Update()
   Future<void> updateFilter(Filter filter);
 
+  @Query('''UPDATE "$filterDb" SET enabled = :enabled''')
+  Future<void> toggleAllFilters(bool enabled);
+
   @Query('''UPDATE "$filterDb" SET enabled = :enabled
-  WHERE imageboard = :imageboard AND tag = :boardTag''')
-  Future<void> toggleAllFilters(
-      bool enabled, String imageboard, String boardTag);
+  WHERE imageboard = :imageboard AND id IN 
+  (SELECT $filterReferenceColumn FROM $filterBoardRelationshipDb
+    WHERE $boardReferenceColumn = :boardId)''')
+  Future<void> toggleAllFiltersForBoard(
+      bool enabled, String imageboard, int boardId);
 
   /// Relationships in relationship table would be deleted by cascade
   @Query('''DELETE FROM "$filterDb" WHERE id = :id''')
   Future<void> deleteFilterById(int id);
-
-  @Query('''DELETE FROM "$filterDb" WHERE id IN 
-    (SELECT $filterReferenceColumn FROM $filterBoardRelationshipDb
-    WHERE $boardReferenceColumn = :id)
-  AND imageboard = :imageboard''')
-  Future<void> deleteFiltersByBoardId(int id, String imageboard);
 
   @Query('DELETE FROM "$filterDb"')
   Future<void> clear();
@@ -59,11 +58,29 @@ abstract class FilterBoardRelationshipDao {
   @Query('''SELECT * from $filterBoardRelationshipDb''')
   Future<List<FilterBoardRelationship>> getRelationships();
 
+  @Query('''SELECT * from $filterBoardRelationshipDb
+  WHERE $filterReferenceColumn = :filterId''')
+  Future<List<FilterBoardRelationship>> getRelationshipsByFilterId(
+      int filterId);
+
   @Query('''
 DELETE FROM $filterBoardRelationshipDb 
 WHERE $filterReferenceColumn = :filterId
 AND $boardReferenceColumn = :boardId''')
   Future<void> deleteRelationshipByForeignKeys(int filterId, int boardId);
+
+  @Query('''
+SELECT * FROM $filterBoardRelationshipDb
+WHERE $boardReferenceColumn = :boardId
+AND (SELECT imageboard 
+  FROM "$filterDb" 
+  WHERE "$filterDb".id = $filterBoardRelationshipDb.$filterReferenceColumn) 
+= :imageboard''')
+  Future<List<FilterBoardRelationship>> getRelationshipsByBoardId(
+      int boardId, String imageboard);
+
+  @delete
+  Future<void> deleteRelationship(FilterBoardRelationship relationship);
 
   @Query('DELETE FROM $filterBoardRelationshipDb')
   Future<void> clear();
