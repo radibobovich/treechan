02c79abe1e6d86971925e3c014f4447303fcc507
radibobovich <radeonovich1@yandex.ru>
2023-11-06 12:15:46 +0300

    register singleton

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -9,15 +9,16 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i10;
+import 'package:dio/dio.dart' as _i11;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:treechan/data/board_fetcher.dart' as _i4;
 import 'package:treechan/data/local/filter_database.dart' as _i3;
 import 'package:treechan/data/local/history_database.dart' as _i6;
-import 'package:treechan/data/rest/rest_client.dart' as _i9;
+import 'package:treechan/data/rest/rest_client.dart' as _i10;
 import 'package:treechan/data/thread/thread_loader.dart' as _i7;
 import 'package:treechan/data/thread/thread_refresher.dart' as _i8;
+import 'package:treechan/domain/services/media_download_service.dart' as _i9;
 import 'package:treechan/utils/constants/enums.dart' as _i5;
 
 const String _prod = 'prod';
@@ -119,12 +120,13 @@ _i1.GetIt init(
       _dev,
     },
   );
-  gh.factoryParam<_i9.RestClient, _i10.Dio, dynamic>(
+  gh.lazySingleton<_i9.MediaDownloadService>(() => _i9.MediaDownloadService());
+  gh.factoryParam<_i10.RestClient, _i11.Dio, dynamic>(
     (
       dio,
       _,
     ) =>
-        _i9.DvachRestClient(dio),
+        _i10.DvachRestClient(dio),
     instanceName: 'dvach',
     registerFor: {
       _test,
@@ -132,12 +134,12 @@ _i1.GetIt init(
       _prod,
     },
   );
-  gh.factoryParam<_i9.RestClient, _i10.Dio, dynamic>(
+  gh.factoryParam<_i10.RestClient, _i11.Dio, dynamic>(
     (
       dio,
       _,
     ) =>
-        _i9.DvachArchiveRestClient(dio),
+        _i10.DvachArchiveRestClient(dio),
     instanceName: 'dvachArchive',
     registerFor: {
       _test,
