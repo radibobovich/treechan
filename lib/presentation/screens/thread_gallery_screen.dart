04ba37b523a2b79b5c31973ae0a72fe718ecfe7a
radibobovich <radeonovich1@yandex.ru>
2023-11-06 12:17:10 +0300

    add selectable

--- lib/presentation/screens/thread_gallery_screen.dart
+++ lib/presentation/screens/thread_gallery_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:treechan/config/themes.dart';
+import 'package:treechan/di/injection.dart';
 import 'package:treechan/domain/models/core/core_models.dart';
 import 'package:treechan/domain/models/tab.dart';
+import 'package:treechan/domain/services/media_download_service.dart';
 import 'package:treechan/presentation/widgets/shared/media_preview_widget.dart';
 
+/// A screen with all the images and videos in the thread.
 class ThreadGalleryScreen extends StatefulWidget {
   const ThreadGalleryScreen({
     super.key,
@@ -22,15 +26,11 @@ class ThreadGalleryScreen extends StatefulWidget {
   State<ThreadGalleryScreen> createState() => _ThreadGalleryScreenState();
 }
 
-class _ThreadGalleryScreenState extends State<ThreadGalleryScreen>
-    with WidgetsBindingObserver {
+class _ThreadGalleryScreenState extends State<ThreadGalleryScreen> {
   List<Widget> media = [];
+  Set<Widget> selected = {};
   late int crossAxisCount;
-  @override
-  void initState() {
-    WidgetsBinding.instance.addObserver(this);
-    super.initState();
-  }
+  Orientation? prevOrientation;
 
   void prepareGrid(Orientation orientation) {
     crossAxisCount = orientation == Orientation.portrait
@@ -39,11 +39,10 @@ class _ThreadGalleryScreenState extends State<ThreadGalleryScreen>
     final double displayWidth = MediaQuery.of(context).size.width;
     final double itemDimension =
         displayWidth / crossAxisCount - widget.spacing * (crossAxisCount - 1);
-    debugPrint(itemDimension.toString());
 
     /// Complete links to full URL's
     final fixedFiles = fixLinks(widget.files, widget.currentTab.imageboard);
-
+    selected.clear();
     media = getMediaItems(fixedFiles, widget.currentTab.imageboard, context,
         height: itemDimension, squareShaped: true);
   }
@@ -57,10 +56,30 @@ class _ThreadGalleryScreenState extends State<ThreadGalleryScreen>
       },
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Галерея'),
+          title: selected.isEmpty
+              ? const Text('Галерея')
+              : Text('Выбрано: ${selected.length}'),
+          actions: [
+            selected.isNotEmpty
+                ? IconButton(
+                    onPressed: () {
+                      final downloader = getIt<MediaDownloadService>();
+                      downloader.downloadMultiple(
+                          selected
+                              .map((e) => (e as MediaItemPreview).file)
+                              .toList(),
+                          imageboard: widget.currentTab.imageboard);
+                    },
+                    icon: const Icon(Icons.download))
+                : const SizedBox.shrink()
+          ],
         ),
         body: OrientationBuilder(builder: (context, orientation) {
-          prepareGrid(orientation);
+          if (prevOrientation != orientation) {
+            prepareGrid(orientation);
+            prevOrientation = orientation;
+          }
+
           return GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: crossAxisCount,
@@ -69,7 +88,34 @@ class _ThreadGalleryScreenState extends State<ThreadGalleryScreen>
             ),
             itemCount: media.length,
             itemBuilder: (context, index) {
-              return media[index];
+              return InkWell(onTap: () {
+                if (selected.isEmpty) return;
+                setState(() {
+                  if (selected.contains(media[index])) {
+                    selected.remove(media[index]);
+                  } else {
+                    selected.add(media[index]);
+                  }
+                });
+              }, onLongPress: () {
+                setState(() {
+                  if (selected.isEmpty) {
+                    selected.add(media[index]);
+                  }
+                });
+              }, child: Builder(builder: (context) {
+                if (selected.contains(media[index])) {
+                  // return const ColoredBox(color: Colors.red);
+                  return Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: context.theme.secondaryHeaderColor,
+                              width: 2)),
+                      child: IgnorePointer(child: media[index]));
+                }
+                return IgnorePointer(
+                    ignoring: selected.isNotEmpty, child: media[index]);
+              }));
             },
           );
         }),
